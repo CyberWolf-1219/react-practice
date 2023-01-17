529c58bb3c12163ff1931379fbb9af825271ad11
cyberwolf <cyberwolf@WolfDen.net>
2023-01-17 07:04:54 +0530

    Style Updates

--- src/components/MemberCard/MemberCard.tsx
+++ src/components/MemberCard/MemberCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styleClasses from "./styles.module.css"
 import { BsFacebook, BsInstagram, BsTwitter, BsLinkedin } from "react-icons/bs";
 
 interface IMemeberCardProps {
@@ -14,55 +15,54 @@ interface IMemeberCardProps {
 
 function MemberCard(props: IMemeberCardProps) {
   return (
-    <div
-      className={`w-full h-auto aspect-[1/1.2] p-4 flex flex-col items-center justify-center gap-4 bg-shade-1 backdrop-blur-sm rounded-lg text-primary shadow-md shadow-slate-800/20`}>
-      <div
-        className={`w-[150px] h-[150px] mx-auto border-4 border-white rounded-full overflow-hidden shadow-md shadow-black/30`}>
-        <img src={props.image} alt="" />
-      </div>
-      <div className={``}>
-        <h4 className={`my-4`}>{props.name}</h4>
-        <h5 className={`my-4`}>{props.role}</h5>
-      </div>
-      <div>{props.bio}</div>
-      <div
-        className={`w-full h-auto mx-auto grid grid-cols-4 items-center justify-items-center`}>
-        {props.facebook ? (
-          <span>
-            <a href={props.facebook}>
-              <BsFacebook size={`1.5rem`} />
-            </a>
-          </span>
-        ) : (
-          ""
-        )}
-        {props.instagram ? (
-          <span>
-            <a href={props.instagram}>
-              <BsInstagram size={`1.5rem`} />
-            </a>
-          </span>
-        ) : (
-          ""
-        )}
-        {props.twitter ? (
-          <span>
-            <a href={props.twitter}>
-              <BsTwitter size={`1.5rem`} />
-            </a>
-          </span>
-        ) : (
-          ""
-        )}
-        {props.linkedin ? (
-          <span>
-            <a href={props.linkedin}>
-              <BsLinkedin size={`1.5rem`} />
-            </a>
-          </span>
-        ) : (
-          ""
-        )}
+    <div className={ styleClasses.card }>
+      <div className={ `w-full h-auto aspect-[1/1.2] p-4 flex flex-col items-center justify-center gap-4 bg-shade-1 hover:bg-white backdrop-blur-sm rounded-lg text-primary shadow-md shadow-slate-800/20` }>
+        <div className={ `z-[1] w-[150px] h-[150px] mx-auto border-4 border-white overflow-hidden shadow-md shadow-black/30` }>
+          <img src={ props.image } alt="" className={ `w-full h-full object-cover object-top` } />
+        </div>
+        <div className={ `z-[2]` }>
+          <h4 className={ `my-4` }>{ props.name }</h4>
+          <h5 className={ `my-4` }>{ props.role }</h5>
+        </div>
+        <div className={ `z-[3]` } >{ props.bio }</div>
+        <div className={ `z-[4] w-full h-auto mx-auto grid grid-cols-4 items-center justify-items-center` }>
+          { props.facebook ? (
+            <span>
+              <a href={ props.facebook }>
+                <BsFacebook size={ `1.5rem` } />
+              </a>
+            </span>
+          ) : (
+            ""
+          ) }
+          { props.instagram ? (
+            <span>
+              <a href={ props.instagram }>
+                <BsInstagram size={ `1.5rem` } />
+              </a>
+            </span>
+          ) : (
+            ""
+          ) }
+          { props.twitter ? (
+            <span>
+              <a href={ props.twitter }>
+                <BsTwitter size={ `1.5rem` } />
+              </a>
+            </span>
+          ) : (
+            ""
+          ) }
+          { props.linkedin ? (
+            <span>
+              <a href={ props.linkedin }>
+                <BsLinkedin size={ `1.5rem` } />
+              </a>
+            </span>
+          ) : (
+            ""
+          ) }
+        </div>
       </div>
     </div>
   );
--- src/components/SectionSix/SectionSix.tsx
+++ src/components/SectionSix/SectionSix.tsx
@@ -6,8 +6,8 @@ import dummyImage from "../../assets/user.jpg";
 function SectionSix() {
   return (
     <section>
-      <Container classes={`p-4 text-center  rounded-lg`}>
-        <div className={`w-full h-fit max-w-screen-sm p-4 mx-auto text-center`}>
+      <Container classes={ `p-4 text-center  rounded-lg` }>
+        <div className={ `w-full h-fit max-w-screen-sm p-4 mx-auto text-center` }>
           <h2>Team</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
@@ -15,56 +15,46 @@ function SectionSix() {
           </p>
         </div>
         <div
-          className={`w-full h-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4`}>
+          className={ `w-full h-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4` }>
           <MemberCard
-            image={dummyImage}
-            name={`Someone Maybe`}
-            role={`Webdeveloper`}
-            bio={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?`}
-            facebook={`www.facebook.com`}
-            instagram={`www.instagram.com`}
-            twitter={`www.twitter.com`}
-            linkedin={`www.linkedin.com`}
+            image={ dummyImage }
+            name={ `Someone Maybe` }
+            role={ `Webdeveloper` }
+            bio={ `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?` }
+            facebook={ `www.facebook.com` }
+            instagram={ `www.instagram.com` }
+            twitter={ `www.twitter.com` }
+            linkedin={ `www.linkedin.com` }
           />
           <MemberCard
-            image={dummyImage}
-            name={`Someone Maybe`}
-            role={`Webdeveloper`}
-            bio={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?`}
-            facebook={`www.facebook.com`}
-            instagram={`www.instagram.com`}
-            twitter={`www.twitter.com`}
-            linkedin={`www.linkedin.com`}
+            image={ dummyImage }
+            name={ `Someone Maybe` }
+            role={ `Webdeveloper` }
+            bio={ `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?` }
+            facebook={ `www.facebook.com` }
+            instagram={ `www.instagram.com` }
+            twitter={ `www.twitter.com` }
+            linkedin={ `www.linkedin.com` }
           />
           <MemberCard
-            image={dummyImage}
-            name={`Someone Maybe`}
-            role={`Webdeveloper`}
-            bio={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?`}
-            facebook={`www.facebook.com`}
-            instagram={`www.instagram.com`}
-            twitter={`www.twitter.com`}
-            linkedin={`www.linkedin.com`}
+            image={ dummyImage }
+            name={ `Someone Maybe` }
+            role={ `Webdeveloper` }
+            bio={ `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?` }
+            facebook={ `www.facebook.com` }
+            instagram={ `www.instagram.com` }
+            twitter={ `www.twitter.com` }
+            linkedin={ `www.linkedin.com` }
           />
           <MemberCard
-            image={dummyImage}
-            name={`Someone Maybe`}
-            role={`Webdeveloper`}
-            bio={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?`}
-            facebook={`www.facebook.com`}
-            instagram={`www.instagram.com`}
-            twitter={`www.twitter.com`}
-            linkedin={`www.linkedin.com`}
-          />
-          <MemberCard
-            image={dummyImage}
-            name={`Someone Maybe`}
-            role={`Webdeveloper`}
-            bio={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?`}
-            facebook={`www.facebook.com`}
-            instagram={`www.instagram.com`}
-            twitter={`www.twitter.com`}
-            linkedin={`www.linkedin.com`}
+            image={ dummyImage }
+            name={ `Someone Maybe` }
+            role={ `Webdeveloper` }
+            bio={ `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quisquam modi corporis tempore quidem aperiam exercitationem ipsum deleniti vel est?` }
+            facebook={ `www.facebook.com` }
+            instagram={ `www.instagram.com` }
+            twitter={ `www.twitter.com` }
+            linkedin={ `www.linkedin.com` }
           />
         </div>
       </Container>
